482416c7e619dd552a50ade52803f004fdd44bac
felipe <felipetavres@gmail.com>
2015-10-12 13:52:18 +0200

    acabado primera iteraccion

--- php/testUnitDBManager.php
+++ php/testUnitDBManager.php
@@ -5,6 +5,23 @@
  <?php
   require "DBManager.php"
   $man = new DBManager;
+
+  function say_error($correct){
+    if($correct){
+      echo "creacion realizada correctamente<br/>";
+    }else{
+      echo "Ya existia un dato asi en la base de datos<br/>";
+    }
+  }
+
   if($man->connect()){
-    
+    say_error($man->insertarRol("rolDummy1","rol de test unit"));
+    say_error($man->insertarFun("funDummy1","funcion de test unit"));
+    say_error($man->insertarPag("pagDummy1","pagina de test unit"));
+    say_error($man->insertarUser("userDummy1","12345","usuario de test unit","dummy@dummy"));
+
+    say_error($man->insertarRol("rolDummy2","rol de test unit"));
+    say_error($man->insertarFun("funDummy2","funcion de test unit"));
+    say_error($man->insertarPag("pagDummy2","pagina de test unit"));
+    say_error($man->insertarUser("userDummy2","12345","usuario de test unit","dummy@dummy"));
   }
