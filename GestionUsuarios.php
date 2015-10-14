0a15392e0df26e21739d30ffe724bc601f62483e
cyanide4all <marvazto@gmail.com>
2015-10-14 21:06:56 +0200

    modificado GestionUsuarios.  Va tomando forma

--- GestionUsuarios.php
+++ GestionUsuarios.php
@@ -2,8 +2,30 @@
   RenderBanner("Gestión de Usuarios");
 ?>
 
-<span class='CajaBusqueda'>Búsqueda Usuarios</span>
+</br></br></br></br>
 
+<?php include("views/lateral.php");
+  renderLateral(0);
+?>
+
+</br></br></br></br>
+<div>
+  <form action="BuscarUsuario.php" method="post">
+
+    <span>Búsqueda Usuarios</span>
+    <input type="text" name="Buscar">
+    <input type="submit">
+
+    <span>Filtrar por rol</span>
+    <select name="Filtrar">
+      <option>Rol1</option>
+      <option>Rol2</option>
+      <option>Rol3</option>
+    </select>
+  </form>
+</div>
+
+</br></br></br></br>
 
 <?php include("views/footer.php");
   renderFooter("Admin"); //esto tiene que ser cambiado por un get de username en php
