c214cf8b32f725e0079d875443daf34af18c3a42
colmilloestelar <rvramos@hotmail.es>
2015-10-15 18:56:09 +0200

    Creados dos nuevos .php para la gestion de funcionalidad
    Primer aporte de raul al fin mira que es puto.

---
+++ gestionfuncionalidad.php
@@ -0,0 +1,20 @@
+<?php include("views/header.php");
+	RenderBanner("Cabeza");
+?>
+<div>
+	<table border=2px >
+		<tr>
+			<th>Funcionalidad</th><th>Descripción</th><th>Eliminar</th>
+		</tr>
+		<tr>
+			<td>Funcionalidad1</td><td>Datos funcion 1</td><td><button>x</button><button>CONF</button></td>
+		</tr>
+	</table>
+	<button>Crear</button>
+	<button>Modificar</button>
+
+
+</div>
+<?php include("views/footer.php");
+	renderFooter("Pie");
+?>
