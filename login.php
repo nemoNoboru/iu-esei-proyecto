7d6b76a6144e96d40b737130b4549c772ffcf81a
cyanide4all <marvazto@gmail.com>
2015-10-15 20:19:59 +0200

    hecho

--- login.php
+++ login.php
@@ -1,1 +1,15 @@
+<?php include("views/header.php");
+	  RenderBanner("Login");
+?>
+    <div id='loginbox'>
+	<div class='T1'>Autentificación</div>
+	<form action="php/ProcesarLogin.php" method="post">
+	ID: <input type="text" name="username"><br>
+	Contraseña: <input type="text" name="pass"><br>
+	<input type="submit" name="login"> 
+	</form>
 
+	</div>
+
+</body>
+</html>
