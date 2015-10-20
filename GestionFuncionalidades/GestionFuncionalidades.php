76b67bdefc114446d6c7e40d1d9743eadbdec85d
cyanide4all <marvazto@gmail.com>
2015-10-20 20:55:03 +0200

    Very cambios, todo parece funcionar, esta noche puede que siga

---
+++ GestionFuncionalidades/GestionFuncionalidades.php
@@ -0,0 +1,26 @@
+<?php include("../views/header.php");
+	RenderBanner("Gestión de Funcionalidades");
+?>
+
+<?php include("../views/lateral.php");
+	RenderLateral(3); //gestion paginas esta el 2 en el array de lateral
+?>
+
+<div id="contenido">
+	<div class="tabla">
+		<table>
+			<tr><th>Funcionalidad</th><th>Descripción</th><th>Eliminar</th></tr>
+			<tr><td>Funcionalidad1</td><td>Datos funcion 1</td><td><button>x</button></tr>
+			<tr><td>Funcionalidad1</td><td>Datos funcion 2</td><td><button>x</button></tr>
+			<tr><td>Funcionalidad1</td><td>Datos funcion 3</td><td><button>x</button></tr>
+			<tr><td>Funcionalidad1</td><td>Datos funcion 4</td><td><button>x</button></tr>
+		</table>
+	</div>
+
+		<button>Crear</button><button>Modificar</button>
+
+</div>
+
+<?php include("../views/footer.php");
+	renderFooter("Admin");
+?>
--- GestionUsuarios/login.php
+++ GestionUsuarios/login.php
@@ -1,9 +1,9 @@
-<?php include("views/header.php");
+<?php include("../views/header.php");
 	  RenderBanner("Login");
 ?>
     <div id='loginbox'>
 	<div class='T1'>Autentificación</div>
-	<form action="php/ProcesarLogin.php" method="post">
+	<form action="../php/GestionUsuarios/ProcesarLogin.php" method="post">
 	ID: <input type="text" name="username"><br>
 	Contraseña: <input type="text" name="pass"><br>
 	<input type="submit" name="login" value="Login">
