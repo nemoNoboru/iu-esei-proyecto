6ae1d1e037d3c3a15e9f4a0d60298e56b30cdf1b
colmilloestelar <rvramos@hotmail.es>
2015-10-21 19:11:41 +0200

    Arreglado bug de idioma
    Ya funciona, lo he llenado toodo de idiomas

--- views/header.php
+++ views/header.php
@@ -7,10 +7,15 @@
     <link href="css/gstr.css" rel="stylesheet">
   </head>
   <body>
-  <?php
+ 
+ <?php
   //Añadido array de Idioma, se debe hacer include Idioma en la Pagina que llame a header.
+
+	
 	function Renderbanner($nombre){
-		echo '<div id="header"> <span class="cabecera">GSTR</span>'.$Idioma[$nombre].'</div>';
+		  include "lenguaje/English.php";
+
+		echo '<div id="header"> <span class="cabecera"> GSTR </span>'.($Idioma[$nombre]).'</div>';
 	}
   ?>
-  
+
