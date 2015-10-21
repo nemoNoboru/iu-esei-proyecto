d6cc5f0193e9edf282600ddd1b916948eeff3907
cyanide4all <marvazto@gmail.com>
2015-10-21 18:10:11 +0200

    ten

--- MenuPrincipal.php
+++ MenuPrincipal.php
@@ -5,11 +5,11 @@
 </br></br></br></br>
 <!-- Cuerpo -->
 
-<div> <!-- Este div llega hasta footer, representa toda la interfaz principal -->
-<div>ROLES</div>
-<div>USUARIOS</div>
-<div>FUNCIONALIDADES</div>
-<div>PÁGINAS</div>
+<div id="cuerpo"> <!-- Este div llega hasta footer, representa toda la interfaz principal -->
+<div class="BotonMenu"><a href="GestionRoles/GestionRoles.php">ROLES</a></div>
+<div class="BotonMenu"><a href="GestionUsuarios/GestionUsuarios.php">USUARIOS</a></div>
+<div class="BotonMenu"><a href="GestionFuncionalidades/GestionFuncionalidades.php">FUNCIONALIDADES</a></div>
+<div class="BotonMenu"><a href="GestionPagina/GestionPaginas.php">PÁGINAS</a></div>
 </div>
 
 </br></br></br></br>
