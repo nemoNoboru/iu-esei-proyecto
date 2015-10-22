c50a946b3c9239eb2c86f5a9a8aed0aa8f6f7698
nemoNoboru <felipetavres@gmail.com>
2015-10-22 23:03:53 +0200

    manejo de sesiones

--- php/GestionUsuarios/ProcesarLogin.php
+++ php/GestionUsuarios/ProcesarLogin.php
@@ -4,11 +4,14 @@ require_once("../DBManager.php");
 //Usamos una funcion de DBManager para comprobar el login
 $man = DBManager::getInstance();
 $man->connect();
+session_start();
+$_SESSION["name"] = "anon";
 if(!$man->tryLogin($_POST["username"],$_POST["pass"])){
   //caso negativo - Vamos a error
   echo "está todo mal";
 }else{
   //caso positivo - Vamos al menu pricipal
+  $_SESSION["name"] = $_POST['username'];
   header("location:../../Menu/MenuPrincipal.php");
 }
 
