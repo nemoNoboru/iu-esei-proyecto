f02863075a1a9364a8087313c70af73fbd85b11d
colmilloestelar <rvramos@hotmail.es>
2015-10-25 05:06:18 +0100

    Idioma ready
    Toma, observa como todo funciona maravillosamente.... esta ricolino, o
    si que placer ver todo funcionar, solo me llevo 20 horas descubrir que
    tenia que invocar a sesion start ijijij son un feder

--- Menu/MenuPrincipal.php
+++ Menu/MenuPrincipal.php
@@ -4,12 +4,29 @@
 <!-- DISCLAIMER: ESTE MENU PRINCIPAL ES PALERISIMO Y SOLO SIRVE PARA TESTEAR ENLACES -->
 </br></br></br></br>
 <!-- Cuerpo -->
+	<?php
+	  switch ($_SESSION["LE"])
+		{
+		case 'sp':
+			include '../views/lenguaje/Spanish.php';
+			break;
+		case 'en':
+			include '../views/lenguaje/English.php';
+			break;
+		case 'br':
+			include '../views/lenguaje/Brasilian.php';
+			break;
+		DEFAULT:
+			include '../views/lenguaje/English.php';
+			break;
+		}
+	?>
 
 <div id="cuerpo"> <!-- Este div llega hasta footer, representa toda la interfaz principal -->
-<div class="BotonMenu"><a href="../GestionRoles/GestionRoles.php">ROLES</a></div>
-<div class="BotonMenu"><a href="../GestionUsuarios/GestionUsuarios.php">USUARIOS</a></div>
-<div class="BotonMenu"><a href="../GestionFuncionalidades/GestionFuncionalidades.php">FUNCIONALIDADES</a></div>
-<div class="BotonMenu"><a href="../GestionPaginas/GestionPaginas.php">PÁGINAS</a></div>
+<div class="BotonMenu"><a href="../GestionRoles/GestionRoles.php"><?php echo $Idioma['Roles']; ?></a></div>
+<div class="BotonMenu"><a href="../GestionUsuarios/GestionUsuarios.php"><?php echo $Idioma['Usuarios']; ?></a></div>
+<div class="BotonMenu"><a href="../GestionFuncionalidades/GestionFuncionalidades.php"><?php echo $Idioma['Funcionalidades']; ?></a></div>
+<div class="BotonMenu"><a href="../GestionPaginas/GestionPaginas.php"><?php echo $Idioma['Paginas']; ?></a></div>
 </div>
 
 </br></br></br></br>
--- views/header.php
+++ views/header.php
@@ -1,4 +1,5 @@
 <!DOCTYPE html>
+
 <html lang="en">
   <head>
     <meta charset="utf-8">
@@ -7,13 +8,41 @@
     <link href="css/gstr.css" rel="stylesheet">
   </head>
   <body>
+  <p>
+	<!-- Estes enlaces invocan a la funcion cambiar idioma enviando la variable $leng con su valor correspondiente-->
+	|<a href="../php/CambioIdioma.php?leng=en"><img src='../views/img/ico_en.png'></a> |
+	<a href="../php/CambioIdioma.php?leng=sp"><img src='../views/img/ico_es.png'></a> |
+	<a href="../php/CambioIdioma.php?leng=br"><img src='../views/img/icono_br.png'></a> |
+  </p>
   <?php
+
   require_once "../views/renderTable.php";
   require_once "../views/renderTableGestion.php";
+  
   //Añadido array de Idioma, se debe hacer include Idioma en la Pagina que llame a header.
 	function Renderbanner($nombre){
-		  include "lenguaje/English.php";
-      session_start();
+		//Cargo la sesion apra tener acceso a los datos.
+     session_start();
+		
+		//Comprobamos el valor de sesion y segun su valor cargo el array con el idioma deseado.
+	  switch ($_SESSION["LE"])
+		{
+		case 'sp':
+			include '../views/lenguaje/Spanish.php';
+			break;
+		case 'en':
+			include '../views/lenguaje/English.php';
+			break;
+		case 'br':
+			include '../views/lenguaje/Brasilian.php';
+			break;
+		DEFAULT:
+			include '../views/lenguaje/English.php';
+			break;
+		}
 		echo '<div id="header"> <span class="cabecera"> GSTR </span>'.($Idioma[$nombre]).'</div>';
+		
+		//Prueba temporal para comprobar que funciona bien.
+		echo 'Sesion: '.$_SESSION["LE"];
 	}
   ?>
