f49633779704b06d7821c234d3738ddad738d948
finn <finn@localhost.localdomain>
2015-10-25 13:55:18 +0100

    added jquery and changed header

--- views/header.php
+++ views/header.php
@@ -3,27 +3,23 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>GSTR</title>
-
+    <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <link href="css/gstr.css" rel="stylesheet">
   </head>
   <body>
-  <p>
-	<!-- Estes enlaces invocan a la funcion cambiar idioma enviando la variable $leng con su valor correspondiente-->
-	|<a href="../php/CambioIdioma.php?leng=en"><img src='../views/img/ico_en.png'></a> |
-	<a href="../php/CambioIdioma.php?leng=sp"><img src='../views/img/ico_es.png'></a> |
-	<a href="../php/CambioIdioma.php?leng=br"><img src='../views/img/icono_br.png'></a> |
-  </p>
   <?php
 
   require_once "../views/renderTable.php";
   require_once "../views/renderTableGestion.php";
-  
+
   //Añadido array de Idioma, se debe hacer include Idioma en la Pagina que llame a header.
 	function Renderbanner($nombre){
 		//Cargo la sesion apra tener acceso a los datos.
      session_start();
-		
+
 		//Comprobamos el valor de sesion y segun su valor cargo el array con el idioma deseado.
 	  switch ($_SESSION["LE"])
 		{
@@ -40,8 +36,19 @@
 			include '../views/lenguaje/English.php';
 			break;
 		}
-		echo '<div id="header"> <span class="cabecera"> GSTR </span>'.($Idioma[$nombre]).'</div>';
-		
+    echo "<nav class='navbar navbar-inverse'>";
+    echo "<div class='container-fluid'>";
+    echo "<span class='navbar-brand'> GSTR </span>";
+    echo "<p class='navbar-text'>".$Idioma[$nombre]."</p>";
+    echo "<ul class='nav navbar-nav'>";
+    echo '<li><a href="../php/CambioIdioma.php?leng=en"><img src="../views/img/ico_en.png"></a>';
+    echo '<li><a href="../php/CambioIdioma.php?leng=en"><img src="../views/img/ico_es.png"></a>';
+    echo '<li><a href="../php/CambioIdioma.php?leng=en"><img src="../views/img/icono_br.png"></a>';
+    echo '</ul>';
+    echo "</div>";
+    echo "</nav>";
+		//echo '<div id="header"> <span class="cabecera"> GSTR </span>'.($Idioma[$nombre]).'</div>';
+
 		//Prueba temporal para comprobar que funciona bien.
 		echo 'Sesion: '.$_SESSION["LE"];
 	}
