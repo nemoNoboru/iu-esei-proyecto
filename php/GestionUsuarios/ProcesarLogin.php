248d88387c8880018bf6ee5b77edc9c7e7dc2c2c
Pgsuarez <pgsuarez@esei.uvigo.es>
2015-10-26 22:46:09 +0100

    Algunos comentarios
    Login, menu principal, gestion de usuarios y procesar login

--- php/GestionUsuarios/ProcesarLogin.php
+++ php/GestionUsuarios/ProcesarLogin.php
@@ -1,18 +1,18 @@
 <?php
 
-require_once("../DBManager.php");
-//Usamos una funcion de DBManager para comprobar el login
-$man = DBManager::getInstance();
-$man->connect();
-session_start();
-$_SESSION["name"] = "anon";
-if(!$man->tryLogin($_POST["username"],$_POST["pass"])){
-  //caso negativo - Vamos a error
+require_once("../DBManager.php"); 							//Se incluye la clase que gestiona la DB
+															//Usamos una funcion de DBManager para comprobar el login
+$man = DBManager::getInstance(); 							//Se creaa un objeto de la calse DBManager, si ya hay alguno creado solo se devuelve el que ya hay.
+$man->connect(); 											//Se conecta con la BD
+session_start();  /Se inicia una sesion
+$_SESSION["name"] = "anon"; 								//Esta linea no hace falta
+if(!$man->tryLogin($_POST["username"],$_POST["pass"])){  	//Se recogen los datos emviados en el formulario de login y se comparan con los datos almacenados en la DB.
+																//caso negativo - Vamos a error
   echo "está todo mal";
 }else{
-  //caso positivo - Vamos al menu pricipal
-  $_SESSION["name"] = $_POST['username'];
-  header("location:../../Menu/MenuPrincipal.php");
+																//caso positivo - Vamos al menu pricipal
+  $_SESSION["name"] = $_POST['username'];  					//Se guarda el nombre del usuario que ha iniciado sesion
+  header("location:../../Menu/MenuPrincipal.php"); 			//Se va al menu principal automaticamente
 }
 
 ?>
