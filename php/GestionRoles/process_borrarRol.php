07cfa186d73cc1da8d809bcc4fb09959bc757b40
cyanide4all <marvazto@gmail.com>
2015-10-26 15:49:41 +0100

    Confirmaciones en los borrados funcionando

--- php/GestionRoles/process_borrarRol.php
+++ php/GestionRoles/process_borrarRol.php
@@ -1,20 +1,27 @@
 <?php
 //Geteamos la id a borrar
 $borrar = $_GET["id"];
-
+$confirm = $_GET["confirm"];
 //Conexion con base de datos
 
 require_once("../DBManager.php");
 $man = DBManager::getInstance(); //crea instancia
 $man->connect(); //conectate a la bbdd
 
-//Borramos
-if($man->borrarRol($borrar)){
-  echo "Borrado Correctamente";
+if($confirm==1){
+  //Borramos
+  if($man->borrarFuncionalidad($borrar)){
+    echo "Borrado Correctamente<br>";
+    echo "<button onclick='location.href=\"../../GestionRoles/GestionRoles.php\"'>OK</button>";
+  }
+  else{
+    echo "todo mal";
+    echo "<button onclick='location.href=\"../../GestionRoles/GestionRoles.php\"'>OK</button>";
+  }
+}else{//Pide confirmación
+  echo "¿Seguro que desea borrar?<br>";
+  echo "<button onclick='location.href=\"../../GestionRoles/GestionRoles.php\"'>Cancelar</button>";
+  echo "<button onclick='location.href=\"../../php/GestionRoles/process_borrarRol.php?id=$borrar&confirm=1\"'>OK</button>";
 }
-else{
-  echo "todo mal";
-}
-//Boton de volver
+
 ?>
-<button onclick="location.href='../../GestionRoles/GestionRoles.php'">OK</button>
