5449a58f01a9cfc39a4b40e7f275032ddbe49c5f
Pgsuarez <pgsuarez@esei.uvigo.es>
2015-10-27 20:25:59 +0100

    Finalizados los comentarios relacionados con alta, baja y consulta de usuarios

--- GestionUsuarios/login.php
+++ GestionUsuarios/login.php
@@ -1,16 +1,18 @@
 <?php include("../views/header.php"); 	//Incluye el header
 	  RenderBanner("Login"); 			//Muestra el header con la funcion definida en header.php
-		$len = getIdioma(); 			//Guarda en $len el array asociativo que almacena el idioma. getIdioma() esta definido en header.php
+		$Idioma = getIdioma(); 			//Guarda en $Idioma el array asociativo que almacena el idioma. getIdioma() esta definido en header.php
 ?>
   <div id='loginbox' class="container">
 		<div class="row">
 			<div class="col-md-4 col-md-offset-4">
-				<div class='lead'><?php echo $len['Iniciar sesion']; ?></div> <!--Muestra el titulo del formulario-->
+				<div class='lead'><?php echo $Idioma['Iniciar sesion']; ?></div> 	<!--Muestra el titulo del formulario-->
+				
 						<!--Formulario de login, los datos se envian a "../php/GestionUsuarios/ProcesarLogin.php"-->
+						
 						<form action="../php/GestionUsuarios/ProcesarLogin.php" method="post">
 							<div class="form-group">
-							<?php echo $len['nick']; ?>: <input class="form-control" type="text" name="username"><br>
-							<?php echo $len['Contraseña']; ?>: <input class="form-control" type="password" name="pass"><br>
+							<?php echo $Idioma['nick']; ?>: <input class="form-control" type="text" name="username"><br>
+							<?php echo $Idioma['Contraseña']; ?>: <input class="form-control" type="password" name="pass"><br>
 							<input type="submit" name="login" value="Login">
 						</div>
 						</form>
--- php/GestionUsuarios/process_crearUsuario.php
+++ php/GestionUsuarios/process_crearUsuario.php
@@ -2,21 +2,25 @@
 /* Procesador de crear usuario (formato modelo)
  * Hecho por FVieira para interfaces de usuario ET1
  */
-if($_POST['pass1']==$_POST['pass2']){
-  require_once("../DBManager.php");
-  $man = DBManager::getInstance(); //crea instancia
-  $man->connect(); //conectate a la bbdd
-  if($man->insertarUser($_POST['nombre'],$_POST['pass1'],$_POST['desc'],$_POST['email'])){//cambiar
-    $roles = $man->listRols();
-    foreach ($roles as $rol) {
-      if(isset($_POST[$rol['rol_name']])){
-        if($man->insertRelationUserRol($_POST['nombre'],$rol['rol_name'])){
-          echo "relacion insertada correctamente";
-        }else{
-          echo "error insertando la relacion";
+if($_POST['pass1']==$_POST['pass2']){	//NECESITA CAMBIARSE PARA USAR JAVASCRIPT					//Se comprueba que las contraseñas introducidas son iguales
+  require_once("../DBManager.php");																	//Se incluye la clase que gestiona la DB
+  $man = DBManager::getInstance();																	//Se creaa un objeto de la calse DBManager, si ya hay alguno creado solo se devuelve el que ya hay.
+  $man->connect(); 																					//Se conecta con la BD
+  if($man->insertarUser($_POST['nombre'],$_POST['pass1'],$_POST['desc'],$_POST['email'])){			//Si se inserta el usuario en la base de datos correctamente
+  
+    //Se insertan las relaciones usuario-rol
+	$roles = $man->listRols();																			//Se obtiene la lista de roles
+    foreach ($roles as $rol) {																			//Se recorren todos los roles
+      if(isset($_POST[$rol['rol_name']])){																//Si el rol actual esta entre los roles que se han enviado en el formulario
+        if($man->insertRelationUserRol($_POST['nombre'],$rol['rol_name'])){									//Si se inserta en la DB la relacion usuario-rolActual correctamente
+          echo "relacion insertada correctamente";																//Mensaje de exito
+        }else{																								//Si no
+          echo "error insertando la relacion";																	//Mensaje de error
         }
       }
     }
+	
+	//Se insertan las relaciones usuario-pagina
     $paginas = $man->listPags();
     foreach ($paginas as $pag) {
       if(isset($_POST[$pag['pag_name']])){
@@ -27,26 +31,28 @@ if($_POST['pass1']==$_POST['pass2']){
         }
       }
     }
+	
+	//Se insertan las relaciones usuario-funcionalidad
     $funcionalidades = $man->listFuns();
     foreach ($funcionalidades as $fun) {
       if(isset($_POST[$fun['fun_name']])){
-        if($man->insertRelationUserRol($_POST['nombre'],$fun['fun_name'])){
+        if($man->insertRelationUserFun$_POST['nombre'],$fun['fun_name'])){
           echo "relacion insertada correctamente";
         }else{
           echo "error insertando la relacion";
         }
       }
     }
-    echo "Usuario creado correctamente";
+    echo "Usuario creado correctamente";																//Mensaje de exito
     // redireccion a mensaje correcto aqui
-  }else{
-    echo "Error creando el usuario, ya existia un usuario con ese nombre";
+  }else{																							//Si no se pudo insertar el usuario
+    echo "Error creando el usuario, ya existia un usuario con ese nombre";								//Mensaje de error
     // redireccion a mensaje de error aqui
   }
-}else {
-  echo "Las contraseñas no coiciden";
+}else {																							//Si no coinciden las contraseñas del formulario
+  echo "Las contraseñas no coiciden";																//Mensaje de error
   //redireccion a mensaje de error aqui
 }
 
 ?>
-<button onclick="location.href='../../GestionUsuarios/GestionUsuarios.php'">OK</button>
+<button onclick="location.href='../../GestionUsuarios/GestionUsuarios.php'">OK</button>			<!--Boton de OK que te devuelve a la gestion de usuarios-->
