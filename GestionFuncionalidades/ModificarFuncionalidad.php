9ec5deac4548c05da693b00e7925640c0cb9a36e
colmilloestelar <rvramos@hotmail.es>
2015-10-30 21:50:48 +0100

    update de modf funcionalidad

--- GestionFuncionalidades/ModificarFuncionalidad.php
+++ GestionFuncionalidades/ModificarFuncionalidad.php
@@ -1,65 +1,70 @@
 <?php include("../views/header.php");
 	RenderBanner("Gestión de Funcionalidades");
+	$Idioma = getIdioma();
 ?>
 
-<?php include("../views/lateral.php");
-	RenderLateral(3);
-?>
 
 <div id="contenido" class="container">
-
-	<form method=post action="../php/GestionFuncinalidades/process_ModificarFunc.php" method="post" id="formulario">
-		<h1>Modificar Funcionalidad</h1>
-		<div>
-		Seleccionar Funcionalidad
-		<select name="SelectFun">
-		   <option value="F1" selected="selected">Funcionalidad 1</option>
-		   <option value="F2">Funcionalidad 2</option>
-		   <option value="F3">Funcionalidad 3</option>
-		   <option value="F4">Funcionalidad 4</option>
-		</select><br>
-		Nombre Funcionalidad:<input type=text name="nombre"><br>
-		Descripcion:<textarea rows="5" cols="30" name="comentarios"></textarea><br>
-		</div>
-
-		<div class="tabla"> <!-- esta tabla va a ser creada dinamicamente en un futuro -->
-			<table>
-				<tr><th>Pagina</th></tr>
-				<tr><td>Pagina1</td><td><input type="checkbox" name="pagina1"/></td></tr>
-				<tr><td>Pagina2</td><td><input type="checkbox" name="pagina2"/></td></tr>
-				<tr><td>Pagina3</td><td><input type="checkbox" name="pagina3"/></td></tr>
-				<tr><td>Pagina4</td><td><input type="checkbox" name="pagina4"/></td></tr>
-			</table>
-		</div>
-
-		<div class="tabla"> <!-- esta tabla va a ser creada dinamicamente en un futuro -->
-			<table>
-				<tr><th>Roles</th></tr>
-				<tr><td>Roles1</td><td><input type="checkbox" name="Roles1"/></td></tr>
-				<tr><td>Roles2</td><td><input type="checkbox" name="Roles2"/></td></tr>
-				<tr><td>Roles3</td><td><input type="checkbox" name="Roles3"/></td></tr>
-				<tr><td>Roles4</td><td><input type="checkbox" name="Roles4"/></td></tr>
-			</table>
-		</div>
-
-		<div class="tabla"> <!-- esta tabla va a ser creada dinamicamente en un futuro -->
-			<table>
-				<tr><th>Usuario</th></tr>
-				<tr><td>Usuario1</td><td><input type="checkbox" name="Usuario1"/></td></tr>
-				<tr><td>Usuario2</td><td><input type="checkbox" name="Usuario2"/></td></tr>
-				<tr><td>Usuario3</td><td><input type="checkbox" name="Usuario3"/></td></tr>
-				<tr><td>Usuario4</td><td><input type="checkbox" name="Usuario4"/></td></tr>
-			</table>
-		</div>
-
-	
-		  <button class="btn btn-default" onclick="history.go(-1)"><?php echo $Idioma['Atras']; ?></button>
-			<input type="button" class="btn btn-default" data-toggle="modal" data-target="#myModal"  value="<?php echo $Idioma['Guardar']; ?>" class="continuar"/>
+	<div class="row">
+		<?php include("../views/lateral.php");
+			RenderLateral(0);
+		?>
+		<div class="col-md-9 col-sm-12">
+			<form method=post action="../php/GestionFuncinalidades/process_ModificarFunc.php" method="post" id="formulario">
+				<div class="form-group">
+			<h1>Modificar Funcionalidad</h1>
+			<div>
+			Seleccionar Funcionalidad
+			<select name="SelectFun">
+			   <option value="F1" selected="selected">Funcionalidad 1</option>
+			   <option value="F2">Funcionalidad 2</option>
+			   <option value="F3">Funcionalidad 3</option>
+			   <option value="F4">Funcionalidad 4</option>
+			</select><br>
+			Nombre Funcionalidad:<input type=text name="nombre"><br>
+			Descripcion:<textarea rows="5" cols="30" name="comentarios"></textarea><br>
 			</div>
-			</form>
-		</div>
-	</div>
-	<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+
+			<div class="tabla"> <!-- esta tabla va a ser creada dinamicamente en un futuro -->
+				<table>
+					<tr><th>Pagina</th></tr>
+					<tr><td>Pagina1</td><td><input type="checkbox" name="pagina1"/></td></tr>
+					<tr><td>Pagina2</td><td><input type="checkbox" name="pagina2"/></td></tr>
+					<tr><td>Pagina3</td><td><input type="checkbox" name="pagina3"/></td></tr>
+					<tr><td>Pagina4</td><td><input type="checkbox" name="pagina4"/></td></tr>
+				</table>
+			</div>
+
+			<div class="tabla"> <!-- esta tabla va a ser creada dinamicamente en un futuro -->
+				<table>
+					<tr><th>Roles</th></tr>
+					<tr><td>Roles1</td><td><input type="checkbox" name="Roles1"/></td></tr>
+					<tr><td>Roles2</td><td><input type="checkbox" name="Roles2"/></td></tr>
+					<tr><td>Roles3</td><td><input type="checkbox" name="Roles3"/></td></tr>
+					<tr><td>Roles4</td><td><input type="checkbox" name="Roles4"/></td></tr>
+				</table>
+			</div>
+
+			<div class="tabla"> <!-- esta tabla va a ser creada dinamicamente en un futuro -->
+				<table>
+					<tr><th>Usuario</th></tr>
+					<tr><td>Usuario1</td><td><input type="checkbox" name="Usuario1"/></td></tr>
+					<tr><td>Usuario2</td><td><input type="checkbox" name="Usuario2"/></td></tr>
+					<tr><td>Usuario3</td><td><input type="checkbox" name="Usuario3"/></td></tr>
+					<tr><td>Usuario4</td><td><input type="checkbox" name="Usuario4"/></td></tr>
+				</table>
+			</div>
+
+		
+			  <button class="btn btn-default" onclick="history.go(-1)"><?php echo $Idioma['Atras']; ?></button>
+				<input type="button" class="btn btn-default" data-toggle="modal" data-target="#myModal"  value="<?php echo $Idioma['Guardar']; ?>" class="continuar"/>
+				</div>
+				</form>
+			</div>
+			</div>
+			</div>
+
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
@@ -76,7 +81,7 @@
     </div>
   </div>
 </div>
-</div>
+
 
 <?php include("../views/footer.php");
 	renderFooter();
