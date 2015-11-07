131357e5be6f6743ea20e7dcfad918c138df03ef
cyanide4all <marvazto@gmail.com>
2015-11-07 20:24:19 +0100

    Arreglada redirección condicional tras pantalla de correcto (Necesaria para cuando un usuario solo puede modificar su pass

--- views/correcto.php
+++ views/correcto.php
@@ -2,13 +2,19 @@
 	  RenderBanner("Login");
 		$len = getIdioma();
 		$checked = $_GET['ID'];
+		$cerb = new Cancerbero("CER_correcto");
+		if($cerb->canAccessPage($_SESSION["name"],"CER_Menu")){
+			$redirect = "../Menu/MenuPrincipal.php";
+		}else{
+			$redirect = "../IUET12015/C_Menu.php";
+		}
 ?>
   <div id='loginbox' class="container">
 		<div class="row">
 			<div class="col-md-4 col-md-offset-4 box">
 				<div class='lead correcto'><?php echo $len['c0']."  "; ?></div>
 				<p class="text-center"><?php echo $len[$checked]; ?></p><br/>
-				<p class="text-center"><a class="btn btn-default" href="../Menu/MenuPrincipal.php"><?php echo $len['Menu Principal']; ?></a></p><br>
+				<p class="text-center"><a class="btn btn-default" href="<?php echo $redirect ?>"><?php echo $len['Menu Principal']; ?></a></p><br>
 			</div>
 		</div>
 	</div>
