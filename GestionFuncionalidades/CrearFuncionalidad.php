0fd526f83932ed9010fd402231d3d1b9de2c4eb8
nemoNoboru <felipetavres@gmail.com>
2015-11-10 22:17:20 +0100

    hotbugfix

--- GestionFuncionalidades/CrearFuncionalidad.php
+++ GestionFuncionalidades/CrearFuncionalidad.php
@@ -39,25 +39,8 @@
 		</form>
 	</div>
 	</div>
-		<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-	  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title" id="myModalLabel"><?php echo $Idioma['Validar']; ?></h4>
-      </div>
-      <div class="modal-body">
-        <?php echo $Idioma['Seguro']; ?>
-      </div>
-      <div class="modal-footer">
-        <a class="btn btn-default" onclick="location.href='GestionFuncionalidades.php'"><?php echo $Idioma['Atras'];?></a>
-        <input type="button" class="btn btn-default btn-primary" data-toggle="modal" data-target="#myModal"  value="<?php echo $Idioma['Guardar']; ?>" class="continuar"/>
-      </div>
-    </div>
-	</div>
-</div>
 <div class="footer logo2"></div>
-
+<?php include("../views/popup.php"); ?>
 <?php include("../views/footer.php");
 	renderFooter(); //aqui va a ir el nombre de usuario de la sesion php
 ?>
